adce498d43a8be10253cb2eacde2999362dee049
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-08-22 16:30:13 +0200

    style(core): reformatting startup file using scalafmt

--- core/src/main/scala/io/github/positionpal/Startup.scala
+++ core/src/main/scala/io/github/positionpal/Startup.scala
@@ -3,22 +3,22 @@ package io.github.positionpal
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.javadsl.Behaviors
 import com.typesafe.config.ConfigFactory
-import org.slf4j.LoggerFactory
+import org.slf4j.{Logger, LoggerFactory}
 
 object Startup:
-  
-  val logger = LoggerFactory.getLogger(Startup.getClass)
-  
+
+  private val logger: Logger = LoggerFactory.getLogger(Startup.getClass)
+
   @main
   def main(args: String*): Unit =
     logger.info("Initializing system")
     initSystem(25525)
-    
-    
-  def initSystem(port: Int): Unit =
+
+  /** Initialize a system on the cluster.
+    * @param port the port where actor should be spawned
+    */
+  private def initSystem(port: Int): Unit =
     val config = ConfigFactory.parseString(s"""
          |akka.remote.artery.canonical.port=$port
-         """.stripMargin)
-      .withFallback(ConfigFactory.load())
+         """.stripMargin).withFallback(ConfigFactory.load())
     ActorSystem[Nothing](Behaviors.empty[Nothing], "chatsystem", config)
-    
