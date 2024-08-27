64e687962adc3722f19f38bd991a1355516928b7
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-08-27 10:32:03 +0200

    feat(entities): define base for Group

---
+++ entities/src/main/scala/messages/Group.scala
@@ -0,0 +1,24 @@
+package messages
+
+import akka.actor.typed.ActorRef
+import akka.pattern.StatusReply
+import akka.serialization.jackson.CborSerializable
+
+/** Input command for the Group entity */
+enum Command extends CborSerializable:
+  case UserEnterToGroup(user: String, replyTo: ActorRef[StatusReply[?]])
+  case UserLeaveFromGroup(user: String, replyTo: ActorRef[StatusReply[?]])
+
+/** Event triggered inside the Group entity */
+enum Event:
+  case UserEnteredToGroupEvent(user: String)
+
+/** Current State maintained on the entity
+  * @param users
+  */
+class State(users: Seq[String]) extends CborSerializable:
+  def addUser(user: String) = State(user +: users)
+  def removeUser(user: String) = State(users.filterNot(_ == user))
+
+object State:
+  def empty: State = State(Seq.empty)
