101a19aa231e817720ba12a2a9d29b4b85b17ed2
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-08-30 10:50:01 +0200

    chore(entities): remove repository

--- entities/build.gradle.kts
+++ entities/build.gradle.kts
@@ -1,10 +1,3 @@
-repositories {
-    mavenCentral()
-    maven {
-        url = uri("https://repo.akka.io/maven")
-    }
-}
-
 dependencies {
     with(rootProject.libs) {
         implementation(akka.serialization.jackson)
