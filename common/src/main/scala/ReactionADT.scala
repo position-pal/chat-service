459b8ca98cac5d95300882f348604d9489b1ccc9
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-09-12 15:21:40 +0200

    build(deps): Add cats library inside the project

--- common/src/main/scala/ReactionADT.scala
+++ common/src/main/scala/ReactionADT.scala
@@ -6,8 +6,17 @@ trait ReactionADT:
   /** Represent the state of an object that could change while handling the execution of the [[Event]]. */
   type State
 
+  /** Represent the result of the reaction */
+  type ReactionResult
+
   /** The context of a Reaction
     * @param event The occurred event
     * @param state The current state of the entity that reacts to the event.
     */
   case class Context(event: Event, state: State)
+
+  /** Executes the reaction on an event
+    * @param context
+    * @return
+    */
+  def on(context: Context): ReactionResult
