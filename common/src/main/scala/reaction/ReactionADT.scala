6a30114630bd3ed51e6e074f35c6cab7c7beab8d
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-09-13 16:18:55 +0200

    feat(common): specialize the ReactionADT in OptionReaction

---
+++ common/src/main/scala/reaction/ReactionADT.scala
@@ -0,0 +1,78 @@
+package reaction
+
+import cats.Monad
+import cats.implicits.toFlatMapOps
+
+/** A data type used for representing a reaction to a given event */
+trait ReactionADT:
+
+  /** Represent something that has occurred inside the system and that should be handled by the Reaction. */
+  type Event
+
+  /** Represent the state of an object that could change while handling the execution of the [[Event]]. */
+  type State
+
+  /** Represent the result of the reaction */
+  type ReactionResult
+
+  /** The context of a Reaction
+    * @param event The occurred event
+    * @param state The current state of the entity that reacts to the event.
+    */
+  case class Context(event: Event, state: State)
+
+  import cats.data.ReaderT
+
+  /** Represent the reaction that's triggered within a specific [[Context]]
+    * @tparam F The effect to apply
+    */
+  opaque type Reaction[F[_]] = ReaderT[F, Context, ReactionResult]
+
+  /** Create a reaction on an given [[Context]]
+    * @param context the context in which the
+    * @return
+    */
+  def on[F[_]](reaction: (context: Context) => F[ReactionResult]): Reaction[F] = ReaderT(reaction)
+
+  extension [F[_]: Monad](reaction: Reaction[F])
+    /** Executes the [[Reaction]] producing a [[ReactionResult]]
+      * @param context The [[Context]] in which the [[Reaction]] should run
+      * @return a functional structure `F` in which the result is contained
+      */
+    def apply(context: Context): F[ReactionResult] = reaction.run(context)
+
+/** A set of operations that compose the execution of two or more [[Reaction]] together */
+trait ComposableReaction extends ReactionADT:
+  extension [F[_]: Monad](reaction: Reaction[F])
+    /** Compose the execution of two [[Reaction]] executing this first and then the [[other]] provided
+      * @param other The [[Reaction]] to execute after this one
+      * @return The new [[Reaction]] that is the combination of the other two
+      */
+    def andThen(other: Reaction[F]): Reaction[F]
+
+/** A set of operations that filter the [[Reaction]] execution */
+trait FilterableReaction extends ReactionADT:
+  extension [F[_]: Monad](reaction: Reaction[F])
+    /** Execute the reaction only if a condition on the [[Context]] is met.
+      * @param predicate A function that takes the [[Context]] and returns `true` if the reaction should be executed.
+      * @return A new [[Reaction]] that only runs if the predicate is satisfied.
+      */
+    def filter(predicate: Context => Boolean): Reaction[F]
+
+/** A specialized version of [[ReactionADT]] where the result of the reaction is wrapped in an `Option`.
+  * This trait provides composition and filtering for reactions that return an `Option[T]`.
+  *
+  * @tparam T The type of the result wrapped in the `Option`.
+  */
+trait OptionReaction[T] extends ComposableReaction with FilterableReaction:
+
+  override type ReactionResult = Option[T]
+
+  extension [F[_]: Monad](reaction: Reaction[F])
+    def andThen(other: Reaction[F]): Reaction[F] = on: context =>
+      reaction(context).flatMap:
+        case Some(_) => other(context)
+        case _ => Monad[F].pure(None)
+
+    def filter(predicate: Context => Boolean): Reaction[F] = on: context =>
+      if predicate(context) then reaction(context) else Monad[F].pure(None)
---
+++ common/src/test/scala/reaction/ReactionTests.scala
@@ -0,0 +1,133 @@
+package reaction
+
+import scala.language.postfixOps
+
+import cats.{Id, Monad}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class ReactionTests extends AnyWordSpecLike with Matchers:
+
+  object TestOptionReaction extends OptionReaction[String]:
+    override type Event = String
+    override type State = Int
+
+  import TestOptionReaction.*
+
+  object Helpers:
+
+    def createReaction(eventResult: Option[String]): Reaction[Id] = on: _ =>
+      Monad[Id].pure(eventResult)
+
+    def eventLengthGreaterThan(minLength: Int): Context => Boolean = { context =>
+      context.event.length > minLength
+    }
+
+    def stateGreaterThan(minState: Int): Context => Boolean = { context =>
+      context.state > minState
+    }
+
+  import Helpers.*
+
+  "OptionReaction" should:
+
+    "concatenate two successful reactions returning some results" in:
+
+      val firstReaction: Reaction[Id] = createReaction(Some("First Success"))
+      val secondReaction: Reaction[Id] = createReaction(Some("Second Success"))
+
+      val chainedReaction: Reaction[Id] = firstReaction.andThen(secondReaction)
+
+      val context = Context(event = "TestEvent", state = 10)
+      val result: Option[String] = chainedReaction(context)
+
+      assert(result.contains("Second Success"))
+
+    "concatenate failing reactions returning empty results" in:
+
+      val firstReaction: Reaction[Id] = createReaction(None)
+      val secondReaction: Reaction[Id] = createReaction(Some("Second Success"))
+
+      val chainedReaction: Reaction[Id] = firstReaction.andThen(secondReaction)
+
+      val context = Context(event = "TestEvent", state = 10)
+      val result: Option[String] = chainedReaction(context)
+
+      assert(result.isEmpty)
+
+    "modify state based on event and return result" in:
+
+      val complexReaction: Reaction[Id] = on { context =>
+        val newState = context.state + context.event.length
+        if (newState > 15) Monad[Id].pure(Some(s"New state is: $newState"))
+        else Monad[Id].pure(None)
+      }
+
+      val context1 = Context(event = "Test", state = 10)
+      val result1: Option[String] = complexReaction(context1)
+      assert(result1.isEmpty)
+
+      val context2 = Context(event = "LongerEvent", state = 10)
+      val result2: Option[String] = complexReaction(context2)
+      assert(result2.contains("New state is: 21"))
+
+    "should not execute second reaction when first fails filter" in:
+      val firstReaction: Reaction[Id] = createReaction(Some("First Success"))
+      val secondReaction: Reaction[Id] = createReaction(Some("Second Success"))
+
+      val filteredFirstReaction = firstReaction.filter(_.state > 5)
+      val chainedReaction = filteredFirstReaction.andThen(secondReaction)
+      val context = Context(event = "TestEvent", state = 3)
+
+      val result: Option[String] = chainedReaction(context)
+      assert(result.isEmpty)
+
+    "work with complex predicates based on both event and state" in:
+      val reaction: Reaction[Id] = createReaction(Some("Complex Success"))
+
+      val complexFilter = reaction.filter: context =>
+        context.event.contains("Allow") && context.state > 5
+
+      val context1 = Context(event = "AllowEvent", state = 10)
+      val result1: Option[String] = complexFilter(context1)
+      assert(result1.contains("Complex Success"))
+
+      val context2 = Context(event = "BlockEvent", state = 10)
+      val result2: Option[String] = complexFilter(context2)
+      assert(result2.isEmpty)
+
+      val context3 = Context(event = "AllowEvent", state = 2)
+      val result3: Option[String] = complexFilter(context3)
+      assert(result3.isEmpty)
+
+    "for-comprehension with filtered reactions should execute correctly" in:
+
+      val reaction1: Reaction[Id] = createReaction(Some("Reaction 1")).filter(eventLengthGreaterThan(5))
+      val reaction2: Reaction[Id] = createReaction(Some("Reaction 2")).filter(stateGreaterThan(10))
+
+      val composedReaction: Reaction[Id] = on { context =>
+        for {
+          result1 <- reaction1(context)
+          result2 <- reaction2(context)
+        } yield result1 + " " + result2
+      }
+
+      val context1 = Context(event = "LongEvent", state = 15)
+      val result1: Option[String] = composedReaction(context1)
+
+      assert(result1.contains("Reaction 1 Reaction 2"))
+
+      val context2 = Context(event = "Short", state = 15)
+      val result2: Option[String] = composedReaction(context2)
+
+      assert(result2.isEmpty)
+
+      val context3 = Context(event = "LongEvent", state = 5)
+      val result3: Option[String] = composedReaction(context3)
+
+      assert(result3.isEmpty)
+
+      val context4 = Context(event = "Short", state = 5)
+      val result4: Option[String] = composedReaction(context4)
+
+      assert(result4.isEmpty)
