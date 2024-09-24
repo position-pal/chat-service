d26a79c8c0d143cc4e320d36a34344558f9865ef
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-09-24 18:10:13 +0200

    build: adding java library plugin in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
+    `java-library`
     id("scala")
     alias(libs.plugins.scala.extras)
 }
@@ -10,8 +11,11 @@ allprojects {
     group = "io.github.positionpal"
 
     with(rootProject.libs.plugins) {
+        apply(plugin = "java-library")
         apply(plugin = "scala")
-        apply(plugin = scala.extras.get().pluginId)
+        if (name != "presentation") {
+            apply(plugin = scala.extras.get().pluginId)
+        }
     }
 
     repositories {
