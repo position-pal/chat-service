670eb134e5b1c0c32a3d5a599b6f5a2bb508b2b6
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-10-18 17:41:28 +0200

    refactor: refactor GroupADT

--- domain/src/main/scala/io/github/positionpal/group/GroupADT.scala
+++ domain/src/main/scala/io/github/positionpal/group/GroupADT.scala
@@ -1,15 +1,42 @@
 package io.github.positionpal.group
 
-import io.github.positionpal.client.ClientADT.Client
+import io.github.positionpal.client.ClientADT.ClientID
 
 object GroupADT:
 
-  trait GroupOps:
-    def addClient(client: Client): GroupOps
-    def clients: List[Client]
+  enum ErrorCode:
+    case ClientAlreadyPresent(clientID: ClientID)
 
-  case class Group(clients: List[Client], name: String) extends GroupOps:
-    override def addClient(client: Client): GroupOps = Group(client :: clients, name)
+  trait GroupOps[O]:
+    /** Add a new client inside a [[GroupOps]]
+      * @param clientID the reference of the client that joins group
+      * @return the updated group
+      */
+    def addClient(clientID: ClientID, outputRef: O): Either[ErrorCode, GroupOps[O]]
+
+    /** List of clients IDs that are inside the group
+      * @return A list of clients that are inside the group
+      */
+    def clientIDList: List[ClientID]
+
+    /** Execute an action on the output reference for a client
+      * @param action The function that must be executed for each client
+      */
+    def executeOnClients(action: O => Unit): Unit
+
+  import ErrorCode.*
+
+  case class Group[O](clients: Map[ClientID, O], name: String) extends GroupOps[O]:
+    override def addClient(clientID: ClientID, outputRef: O): Either[ErrorCode, GroupOps[O]] =
+      if clients isDefinedAt clientID then Left(ClientAlreadyPresent(clientID))
+      else Right(Group(clients + (clientID -> outputRef), name))
+
+    override def clientIDList: List[ClientID] = clients.keys.toList
+    override def executeOnClients(action: O => Unit): Unit = clients.values.foreach(action)
 
   object Group:
-    def empty(name: String): Group = Group(List.empty, name)
+    /** Return a group without clients inside of it
+      * @param name the name of the group you're creating
+      * @return A new [[Group]]
+      */
+    def empty[O](name: String): Group[O] = Group(Map.empty, name)
--- domain/src/test/scala/io/github/positionpal/group/GroupADTTest.scala
+++ domain/src/test/scala/io/github/positionpal/group/GroupADTTest.scala
@@ -1,16 +1,51 @@
 package io.github.positionpal.group
 
-import io.github.positionpal.client.ClientADT.ClientStatus.*
-import io.github.positionpal.client.ClientADT.{Client, ClientID}
+import io.github.positionpal.client.ClientADT.ClientID
+import io.github.positionpal.group.GroupADT.ErrorCode.ClientAlreadyPresent
 import io.github.positionpal.group.GroupADT.Group
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
 
 class GroupADTTest extends AnyWordSpecLike with Matchers:
+
+  trait ExternalRefOps:
+    def containedString: String
+    def executeCommand(command: String => String): ExternalRefOps
+
+  case class StringContainer(containedString: String) extends ExternalRefOps:
+    override def executeCommand(command: String => String): ExternalRefOps = StringContainer(command(containedString))
+
   "Group" should:
     "add a new client" in:
-      val group = Group.empty("test")
-      val clientID = ClientID("123", "prova.email@abc.it")
-      val client = Client(clientID, ONLINE)
-      val newGroup = group.addClient(client)
-      assert(newGroup.clients.contains(client))
+      val group = Group.empty[ExternalRefOps]("testGroup")
+      val container = StringContainer("testString")
+      val clientID = ClientID("1b23", "email@prova.it")
+      val updatedGroup = group.addClient(clientID, container)
+      updatedGroup match
+        case Right(group @ Group(_, _)) => assert(group.clientIDList.contains(clientID))
+        case _ => fail("Error while trying to insert a client in a group")
+
+    "return a error code while trying to insert a client with the same ClientID" in:
+      val group = Group.empty[ExternalRefOps]("testGroup")
+      val container = StringContainer("testString")
+      val container2 = StringContainer("testString2")
+      val clientID = ClientID("1b23", "email@prova.it")
+      val updatedGroup = for
+        grp1 <- group.addClient(clientID, container)
+        grp2 <- grp1.addClient(clientID, container2)
+      yield grp2
+
+      updatedGroup match
+        case Left(ClientAlreadyPresent(cl)) => assert(cl == clientID)
+        case _ => fail("Error while trying to insert a client in a group")
+
+    "execute an operation on all the external references of the group" in:
+      val group = Group.empty[ExternalRefOps]("testGroup")
+      val container = StringContainer("testString")
+      val clientID = ClientID("1b23", "email@prova.it")
+      group.addClient(clientID, container) match
+        case Right(grp @ Group(_, _)) =>
+          grp.executeOnClients: externalRef =>
+            val newRef = externalRef.executeCommand(_.toUpperCase)
+            assert(newRef.containedString == "TESTSTRING")
+        case _ => fail("Error while trying to insert a client in a group")
