7f8e45ba42ecd99776b0b2850a3ad268bbb5640a
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-10-29 23:48:24 +0100

    build(deps): adding borer serializer

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -2,5 +2,7 @@ dependencies {
     api(project(":application"))
     with(rootProject.libs) {
         implementation(akka.serialization.jackson)
+        implementation(borer.core)
+        implementation(borer.derivation)
     }
 }
