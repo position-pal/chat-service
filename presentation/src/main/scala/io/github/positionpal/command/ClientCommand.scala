5e2a0f5a63f5524877ab8a7c82764cacc49624d3
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-11-02 15:39:41 +0100

    chore(presentation): removed ex client's event and command files

--- presentation/src/main/scala/io/github/positionpal/command/ClientCommand.scala
+++
@@ -1,6 +0,0 @@
-package io.github.positionpal.command
-import akka.actor.typed.ActorRef
-import akka.serialization.jackson.CborSerializable
-
-sealed trait ClientCommand extends CborSerializable
-case class ReceiveReference(ref: ActorRef[?]) extends ClientCommand
--- presentation/src/main/scala/io/github/positionpal/event/ClientEvent.scala
+++
@@ -1,7 +0,0 @@
-package io.github.positionpal.event
-
-import akka.actor.typed.ActorRef
-import akka.serialization.jackson.CborSerializable
-
-sealed trait ClientEvent extends CborSerializable
-case class NewReference(ref: ActorRef[?]) extends ClientEvent
