75d51539d44cb49ece2c2cd5f6d8d69e77c9e0e2
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-11-29 19:17:00 +0100

    refactor(amqp): change basic handler implementation calling GroupService methods for operations

--- amqp/src/main/scala/io/github/positionpal/handler/Handlers.scala
+++ amqp/src/main/scala/io/github/positionpal/handler/Handlers.scala
@@ -1,31 +1,47 @@
 package io.github.positionpal.handler
 
+import akka.actor.typed.ActorSystem
 import akka.util.ByteString
 import cats.Id
+import io.github.positionpal.client.ClientID
+import io.github.positionpal.service.GroupService
+import io.github.positionpal.services.GroupHandlerService
 import io.github.positionpal.{AvroSerializer, MessageType}
 
 object Handlers:
   private val serializer = AvroSerializer()
 
-  def basic: MessageHandler[Id] = (messageType: MessageType, message: ByteString) =>
+  def basic(using actorSystem: ActorSystem[?]): MessageHandler[Id] = (messageType: MessageType, message: ByteString) =>
+    val service: GroupHandlerService = GroupService(actorSystem)
     val byteArray = message.toArray
+
     messageType match
       case MessageType.GROUP_CREATED =>
-        val result = serializer.deserializeGroupCreated(byteArray)
-        println(result)
-        result
+        val deserializedEvent = serializer.deserializeGroupCreated(byteArray)
+
+        val user = deserializedEvent.createdBy()
+        val groupId = deserializedEvent.groupId()
+
+        service.join(groupId)(ClientID(user.id()))
 
       case MessageType.GROUP_DELETED =>
-        val result = serializer.deserializeGroupDeleted(byteArray)
-        println(result)
-        result
+        val deserializedEvent = serializer.deserializeGroupDeleted(byteArray)
+        val groupId = deserializedEvent.groupId()
+
+        groupId
 
       case MessageType.MEMBER_ADDED =>
-        val result = serializer.deserializeAddedMemberToGroup(byteArray)
-        println(result)
-        result
+        val deserializedEvent = serializer.deserializeAddedMemberToGroup(byteArray)
+
+        val user = deserializedEvent.addedMember()
+        val groupId = deserializedEvent.groupId()
+
+        service.join(groupId)(ClientID(user.id()))
 
       case MessageType.MEMBER_REMOVED =>
-        val result = serializer.deserializeRemovedMemberToGroup(byteArray)
-        println(result)
-        result
+        val deserializedEvent = serializer.deserializeRemovedMemberToGroup(byteArray)
+
+        val user = deserializedEvent.removedMember()
+        val groupId = deserializedEvent.groupId()
+
+        service.leave(groupId)(ClientID(user.id()))
